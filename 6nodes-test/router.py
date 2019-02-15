0d01df09053e8bbba25a9dcd45e0482c2cb47662
Qi <zhaoq0563@gmail.com>
2019-02-15 07:25:13 -0800

    Add new testing of switching two middle OVS to linux router

--- 6nodes-test/router.py
+++ 6nodes-test/router.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python
 
 from mininet.net import Mininet
-from mininet.node import Controller
+from mininet.node import Controller, Node
 from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel
 
 import os, time, threading
+
+""" Router class definition """
+
+class LinuxRouter( Node ):
+    "A Node with IP forwarding enabled."
+
+    def config( self, **params ):
+        super( LinuxRouter, self).config( **params )
+        # Enable forwarding on the router
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+
+    def terminate( self ):
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        super( LinuxRouter, self ).terminate()
 
 """ Application related function """
 
@@ -23,8 +37,8 @@ def sendingIperfTraffic(nodes, loc):
     print "*** Starting iPerf Clients on stations ***"
     time.sleep(1)
     client = nodes['h1']
-    totalTime = 10
-    client.cmdPrint('iperf -c 10.0.0.2 -t '+str(totalTime))
+    totalTime = 60
+    client.cmdPrint('iperf -c 10.0.2.2 -t '+str(totalTime))
 
 """ Main function of the simulation """
 
@@ -43,25 +57,24 @@ def mobileNet(loc, loss, conges, delay):
     print("*** Creating nodes ***")
     nodes = {}
 
-    node = net.addHost('h1')
+    node = net.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/24')
+    nodes['r1'] = node
+    node = net.addHost('r2', cls=LinuxRouter, ip='10.0.0.2/24')
+    nodes['r2'] = node
+    node = net.addHost('h1', ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
     nodes['h1'] = node
-    node = net.addHost('h2')
+    node = net.addHost('h2', ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
     nodes['h2'] = node
     node = net.addSwitch('s1')
     nodes['s1'] = node
     node = net.addSwitch('s2')
     nodes['s2'] = node
-    node = net.addSwitch('s3')
-    nodes['s3'] = node
-    node = net.addSwitch('s4')
-    nodes['s4'] = node
 
+    net.addLink(nodes['r1'], nodes['r2'], intfName1='r1-eth0', intfName2='r2-eth0', bw=10, delay=str(delay)+'ms', loss=float(loss)*100)
+    net.addLink(nodes['s1'], nodes['r1'], intfName2='r1-eth1')
+    net.addLink(nodes['s2'], nodes['r2'], intfName2='r2-eth1')
     net.addLink(nodes['h1'], nodes['s1'])
-    net.addLink(nodes['s1'], nodes['s2'])
-    net.addLink(nodes['s2'], nodes['s3'], bw=10, delay=str(delay)+'ms', loss=float(loss)*100)
-    # net.addLink(nodes['s2'], nodes['s3'], bw=10)
-    net.addLink(nodes['s3'], nodes['s4'])
-    net.addLink(nodes['s4'], nodes['h2'])
+    net.addLink(nodes['h2'], nodes['s2'])
 
     node = net.addController('c0')
     nodes['c0'] = node
@@ -69,15 +82,29 @@ def mobileNet(loc, loss, conges, delay):
     print("*** Starting network simulation ***")
     net.start()
 
+    # Addressing and routing
+    r1, r2 = [nodes['r1'], nodes['r2']]
+    r1.cmd('ip addr add 10.0.1.1/24 dev r1-eth1')
+    r1.cmd('../simpletun/netconfig.sh set tunt1 192.168.0.1/24')
+    r1.cmd('ip route add 10.0.2.0/24 via 192.168.0.1')
+    r2.cmd('ip addr add 10.0.2.1/24 dev r2-eth1')
+    r2.cmd('../simpletun/netconfig.sh set tunt2 192.168.0.2/24')
+    r2.cmd('ip route add 10.0.1.0/24 via 192.168.0.2')
+
+    # Launching the forwarder to initiate the tunnel
+    r2.cmd('../simpletun/simpletun -i tunt2 -s &')
+    r1.cmd('../simpletun/simpletun -i tunt1 -c 10.0.0.2 &')
+
+    # Adding tap interfaces for connecting VMs
     print("*** Configuring virtual ports for VMs ***")
     os.system('sudo ip tuntap add mode tap vport1')
     os.system('sudo ip tuntap add mode tap vport2')
     os.system('sudo ifconfig vport1 up')
     os.system('sudo ifconfig vport2 up')
     os.system('sudo ovs-vsctl add-port s1 vport1')
-    os.system('sudo ovs-vsctl add-port s4 vport2')
+    os.system('sudo ovs-vsctl add-port s2 vport2')
 
-    CLI(net)
+    # CLI(net)
 
     print "*** Starting to generate the traffic ***"
     traffic_thread = threading.Thread(target=sendingIperfTraffic, args=(nodes, loc))
@@ -86,12 +113,13 @@ def mobileNet(loc, loss, conges, delay):
 
     print("*** Stopping network ***")
     os.system('sudo ovs-vsctl del-port s1 vport1')
-    os.system('sudo ovs-vsctl del-port s4 vport2')
+    os.system('sudo ovs-vsctl del-port s2 vport2')
     os.system('sudo ifconfig vport1 down')
     os.system('sudo ifconfig vport2 down')
     os.system('sudo ip link delete vport1')
     os.system('sudo ip link delete vport2')
     net.stop()
+
 
 
 if __name__ == '__main__':
--- simpletun/netconfig.sh
+++ simpletun/netconfig.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-name="tunt2"
-ipaddr="192.168.0.4/24"
-
+if [ $# -lt 3 ];then
+	echo "Usage: $0 [set | clear] [interface name] [ip-addr/mask]"
+	exit 1
+fi
 
 if [ "$1" == "set" ];then
-        sudo ip tuntap add mode tun $name
-        sudo ifconfig $name up
-        sudo ip addr add $ipaddr dev $name
+    sudo ip tuntap add mode tun $2
+    sudo ifconfig $2 up
+    sudo ip addr add $3 dev $2
 elif [ "$1" == "clear" ];then
-        sudo ip addr del $ipaddr dev $name
-        sudo ifconfig $name down
-        sudo ip tuntap del mode tun $name
+    sudo ip addr del $3 dev $2
+    sudo ifconfig $2 down
+    sudo ip tuntap del mode tun $2
 else
-        echo "Must give an operation: set or clear!"
+    echo "Must give an operation: set or clear!"
 fi
 
